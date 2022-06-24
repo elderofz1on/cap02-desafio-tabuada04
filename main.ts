75bec50c2f4315f04a0f15367b26e2794acd447f
Wagner Rodrigues <wprodriguesbr@gmail.com>
2022-06-24 16:38:05 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+// alterna entre os niveis de dificuldade
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showString("<P  R>")
+let operacao = randint(0, 3)
+let nivel = 0
